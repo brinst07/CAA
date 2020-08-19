c280381674ff7d5076da8ef8b9ca2552251436e8
Jeon <jyh6842@gmail.com>
2020-08-19 13:51:44 +0900

    Controller 위치 이전

--- src/main/java/kr/or/ddit/caa/controller/CAAController.java
+++ src/main/java/kr/or/ddit/caa/controller/CAAController.java
@@ -7,9 +7,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/caa/*")
 public class CAAController {
-	
+
 	@GetMapping("/caaSelect")
 	public String caaSelect() {
 		return "caa/caa/selectPage";
 	}
+
+	/* 영현  */
+	@GetMapping("/iRecommendation")
+	public String IRtest() {
+		return "caa/ir/iRecommendation";
+	}
+
+	@GetMapping("/iRecommendation2")
+	public String IRtest2() {
+		return "caa/ir/iRecommendation2";
+	}
+
+	@GetMapping("/iRecommendation3")
+	public String IRtest3() {
+		return "caa/ir/iRecommendation3";
+	}
+
+	@GetMapping("/ss")
+	public String sS() {
+		return "caa/ss/sS";
+	}
+
 }
