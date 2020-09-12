7e742878a0bdb7d0615da7c4b6bf63928f6d5445
Jeon <jyh6842@gmail.com>
2020-09-12 17:34:36 +0900

    업종 추천 지역 레벨 가져오기 작업중 DB 작업중

--- src/main/java/kr/or/ddit/ir/controller/RecommendController.java
+++ src/main/java/kr/or/ddit/ir/controller/RecommendController.java
@@ -1,19 +1,23 @@
 package kr.or.ddit.ir.controller;
 
+import kr.or.ddit.ir.domain.SectorsLargeLevelVO;
 import kr.or.ddit.ir.service.RecommandService;
 import lombok.extern.log4j.Log4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 @Log4j
 @RequestMapping("/ir/*")
 public class RecommendController {
 
-
+    @Autowired
     private RecommandService service;
 
 
@@ -26,6 +30,7 @@ public class RecommendController {
     @GetMapping("/recommendSectorsLevel")
     public String caaArea(@RequestParam("areaName") String sales_bd_name, Model model) {
 
+        List<SectorsLargeLevelVO> list = service.getSectorsLargeLevel(sales_bd_name);
         model.addAttribute("SectorsLargeLevel",service.getSectorsLargeLevel(sales_bd_name));
         model.addAttribute("SectorsMiddleLevel",service.getSectorsMiddleLevel(sales_bd_name));
 
