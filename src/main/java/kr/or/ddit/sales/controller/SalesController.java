cefd42a4fda8004921e052d4a44ba5b84c58e589
Jeon <jyh6842@gmail.com>
2020-09-17 09:22:54 +0900

    salesController 수정

--- src/main/java/kr/or/ddit/sales/controller/SalesController.java
+++ src/main/java/kr/or/ddit/sales/controller/SalesController.java
@@ -11,28 +11,26 @@ import kr.or.ddit.sales.service.SalesService;
 @Controller
 @RequestMapping("/sales/*")
 public class SalesController {
-	
+
 	@Autowired
 	private SalesService service;
-	
-	/* 영현  */
 
-	/* ↓ 매출 통계  영현 */
+	/* 영현 */
+
+	/* ↓ 매출 통계 영현 */
 	@GetMapping("/salesBoard")
 	public String salesList(Model model) {
-		
+
 		// 지역 선택 코드
 		model.addAttribute("SidoList", service.getSidoList());
 		model.addAttribute("SigunguList", service.getSigunguList("1111"));
-		
+
 		// 대분류 코드
 		model.addAttribute("csCodeList", service.getCscodeList("1"));
 		model.addAttribute("csCodeList2", service.getCscodeList2("a"));
-		
+
 		return "caa/sales/salesBoard";
 	}
 	/* ↑ 매출 통계 */
-	
-	
-	
+
 }
