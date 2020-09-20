f32691bda411da5e5ccbbcfaa238b6c5632c6ad3
park <brinst07@gmail.com>
2020-09-20 14:27:19 +0900

    트위터 => 네이버 블로그 크롤링

--- src/main/java/kr/or/ddit/sns/controller/SNSRestController.java
+++ src/main/java/kr/or/ddit/sns/controller/SNSRestController.java
@@ -1,5 +1,9 @@
 package kr.or.ddit.sns.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.twitter.penguin.korean.TwitterKoreanProcessorJava;
 import com.twitter.penguin.korean.tokenizer.KoreanTokenizer;
 import kr.or.ddit.sns.domain.WordVO;
@@ -17,7 +21,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import scala.collection.Seq;
-import twitter4j.*;
+import twitter4j.Query;
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
 import twitter4j.conf.ConfigurationBuilder;
 
 import java.io.IOException;
@@ -28,118 +34,123 @@ import java.util.*;
 @Log4j
 @AllArgsConstructor
 public class SNSRestController {
-	
-	@Autowired
-	private DataLab datalab;
-	@Autowired
-	private TextAnalysis text;
-	
-	@PostMapping("/wordCloud")
-	public ResponseEntity<Map<String, Object>> wordCloud(@RequestBody WordVO vo) {
-		String url = "https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query=" + vo.getKeyword();
 
-		Document doc;
-		List<String> keywords = null;
-		try {
-			keywords = new ArrayList<>();
-			doc = Jsoup.connect(url).get();
-			Elements block = doc.getElementsByClass("_related_keyword_ul");
-			Elements li = block.select("li");
-			// 연관검색어를 리스트에 넣는 부분
-			for (Element element : li) {
-				keywords.add(element.text());
-			}
+    @Autowired
+    private DataLab datalab;
+    @Autowired
+    private TextAnalysis text;
+    @Autowired
+    private NaverBlog naverBlog;
 
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+    @PostMapping("/wordCloud")
+    public ResponseEntity<Map<String, Object>> wordCloud(@RequestBody WordVO vo) {
+        String url = "https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query=" + vo.getKeyword();
 
-		ConfigurationBuilder cb = new ConfigurationBuilder();
-		cb.setDebugEnabled(true).setOAuthConsumerKey("7Q6pWnroTBFRhN2dLGK6WwuhU")
-				.setOAuthConsumerSecret("wEysjZcsvWtDzoDnRa1loIlgwZkPqcnN0C3XDNqTM9a59D7sJq")
-				.setOAuthAccessToken("1300263521997922305-llMTk2omFPmjgnMXggIr0PkoDQmfbA")
-				.setOAuthAccessTokenSecret("84wqzksAT4h0sm3rHXBqMKn9Qk3AiVyYn4EgB9aCedNT1");
+        Document doc;
+        List<String> keywords = null;
+        try {
+            keywords = new ArrayList<>();
+            doc = Jsoup.connect(url).get();
+            Elements block = doc.getElementsByClass("_related_keyword_ul");
+            Elements li = block.select("li");
+            // 연관검색어를 리스트에 넣는 부분
+            for (Element element : li) {
+                keywords.add(element.text());
+            }
 
-		TwitterFactory tf = new TwitterFactory(cb.build());
-		Twitter twitter = tf.getInstance();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
-		Query query = new Query(vo.getKeyword());
-		query.count(100);
-		QueryResult result = null;
-		List<Status> tweets = null;
-		String resultTxt = "";
-		List<Map<String, Object>> list = new ArrayList<>();
-		Map<String, Object> emotial = new HashMap<String,Object>();
-		try {
-			result = twitter.search(query);
-			tweets = result.getTweets();
-			List<Map<String,String>> emotialData = new ArrayList<Map<String,String>>();
-			for (int i = 0; i < tweets.size(); i++) {
-				resultTxt += tweets.get(i).getText();
-				Map<String, String> emotialMap = new HashMap<String, String>();
-				
-			}
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true).setOAuthConsumerKey("7Q6pWnroTBFRhN2dLGK6WwuhU")
+                .setOAuthConsumerSecret("wEysjZcsvWtDzoDnRa1loIlgwZkPqcnN0C3XDNqTM9a59D7sJq")
+                .setOAuthAccessToken("1300263521997922305-llMTk2omFPmjgnMXggIr0PkoDQmfbA")
+                .setOAuthAccessTokenSecret("84wqzksAT4h0sm3rHXBqMKn9Qk3AiVyYn4EgB9aCedNT1");
 
-		} catch (TwitterException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		//---------------------------------------감정분석
-		text.emotionAnalysis(resultTxt);
-		
-		
-		
-		
-		
-		//---------------------------------------감정분석
-		
+        TwitterFactory tf = new TwitterFactory(cb.build());
+        Twitter twitter = tf.getInstance();
 
-		String response = datalab.start(vo.getStartDate(), vo.getEndDate(), vo.getKeyword());
-		log.info(response);
-		
-		CharSequence normalized = TwitterKoreanProcessorJava.normalize(resultTxt);
-		Seq<KoreanTokenizer.KoreanToken> tokens = TwitterKoreanProcessorJava.tokenize(normalized);
-		tokens = TwitterKoreanProcessorJava.stem(tokens);
-		List<String> javaParsed = TwitterKoreanProcessorJava.tokensToJavaStringList(tokens);
-		
-		// 단어 Counting
-		Set<String> wordSet = new HashSet<String>();
-		Map<String, Integer> wordCnt = new HashMap<String, Integer>();
-		for (String s : javaParsed) {
-			// 일반적으로 많이 등장하는 단어 : 단어 리스트에 불포함
-			if (s.equals("으로") || s.equals("에서") || s.equals("이다") || s.equals("있다") || s.equals("일본") || s.equals("되다") || s.equals("RT") || s.equals("알티") || s.equals("하다"))
-				continue;
-			// 단어 길이가 1인 조사들 : 단어 리스트에 불포함
-			if (s.length() > 1) {
-				if (wordSet.contains(s)) {
-					wordCnt.put(s, 1 + wordCnt.get(s));
-				} else if (wordSet.isEmpty()) {
-					wordCnt.put(s, 1);
-				} else {
-					wordCnt.put(s, 1);
-				}
-				wordSet.add(s);
-			}
-		}
+        Query query = new Query(vo.getKeyword());
+        ObjectMapper mapper = new ObjectMapper();
+        String naver = naverBlog.naverBlogcrawling(vo.getKeyword());
+        JsonParser jsonParser = new JsonParser();
+        JsonObject jsonObject = (JsonObject) jsonParser.parse(naver);
+        JsonArray array = jsonObject.get("items").getAsJsonArray();
+        String resultTxt = "";
+        List<Map<String, Object>> list = new ArrayList<>();
+        for(int i = 0; i<array.size(); i++){
+            resultTxt += array.get(i).getAsJsonObject().get("title");
+            resultTxt += array.get(i).getAsJsonObject().get("description");
+        }
 
-		// Json 형태로 만들기
-		for (String temp : wordSet) {
-			if (wordCnt.get(temp) < 10)
-				continue;
-			else {
-				Map<String, Object> map = new LinkedHashMap<String, Object>();
-				map.put("text", temp);
-				map.put("size", wordCnt.get(temp));
-				list.add(map);
-			}
-		}
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("list", list);
-		map.put("keywords",keywords);
-		map.put("response", response);
-		return new ResponseEntity<Map<String,Object>>(map,HttpStatus.OK);
+//        query.count(100);
+//        QueryResult result = null;
+//        List<Status> tweets = null;
+//        String resultTxt = "";
+//        List<Map<String, Object>> list = new ArrayList<>();
+//        Map<String, Object> emotial = new HashMap<String, Object>();
+//        try {
+//            result = twitter.search(query);
+//            tweets = result.getTweets();
+//            List<Map<String, String>> emotialData = new ArrayList<Map<String, String>>();
+//            for (int i = 0; i < tweets.size(); i++) {
+//                resultTxt += tweets.get(i).getText();
+//                Map<String, String> emotialMap = new HashMap<String, String>();
+//
+//            }
+//
+//        } catch (TwitterException e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        }
 
-	}
+
+
+        String response = datalab.start(vo.getStartDate(), vo.getEndDate(), vo.getKeyword());
+
+        CharSequence normalized = TwitterKoreanProcessorJava.normalize(resultTxt);
+        Seq<KoreanTokenizer.KoreanToken> tokens = TwitterKoreanProcessorJava.tokenize(normalized);
+        tokens = TwitterKoreanProcessorJava.stem(tokens);
+        List<String> javaParsed = TwitterKoreanProcessorJava.tokensToJavaStringList(tokens);
+
+        // 단어 Counting
+        Set<String> wordSet = new HashSet<String>();
+        Map<String, Integer> wordCnt = new HashMap<String, Integer>();
+        for (String s : javaParsed) {
+            // 일반적으로 많이 등장하는 단어 : 단어 리스트에 불포함
+            if (s.equals("으로") || s.equals("에서") || s.equals("이다") || s.equals("있다") || s.equals("일본") || s.equals("되다") || s.equals("RT") || s.equals("\"") || s.equals("하다"))
+                continue;
+            // 단어 길이가 1인 조사들 : 단어 리스트에 불포함
+            if (s.length() > 1) {
+                if (wordSet.contains(s)) {
+                    wordCnt.put(s, 1 + wordCnt.get(s));
+                } else if (wordSet.isEmpty()) {
+                    wordCnt.put(s, 1);
+                } else {
+                    wordCnt.put(s, 1);
+                }
+                wordSet.add(s);
+            }
+        }
+
+        // Json 형태로 만들기
+        for (String temp : wordSet) {
+            if (wordCnt.get(temp) < 10)
+                continue;
+            else {
+                Map<String, Object> map = new LinkedHashMap<String, Object>();
+                map.put("text", temp);
+                map.put("size", wordCnt.get(temp));
+                list.add(map);
+            }
+        }
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("list", list);
+        map.put("keywords", keywords);
+        map.put("response", response);
+        return new ResponseEntity<Map<String, Object>>(map, HttpStatus.OK);
+
+    }
 }
